65ac5d74d2502ef22851ba46d04e9f447bc781d6
Илко Илиев <iciservicebg@gmail.com>
2025-03-11 10:33:40 +0200

    Add AcoountController

---
+++ MyAuthApp/Models/Account/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyAuthApp.Models.Account
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Family Name")]
+        public string FamilyName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Двете пароли не съвпадат!")]
+        public string ConfirmPassword { get; set; }
+    }
+}
